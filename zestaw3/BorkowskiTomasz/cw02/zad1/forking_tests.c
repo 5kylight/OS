adb5550f9c34501e63ce4b145f5ee74c1a089465
BorkowskiTomasz215 <BorkowskiTomasz215@gmail.com>
2015-04-03 20:50:40 +0200

    Zad 1 updated

--- zestaw3/BorkowskiTomasz/cw02/zad1/forking_tests.c
+++ zestaw3/BorkowskiTomasz/cw02/zad1/forking_tests.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,92 +7,131 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/times.h>
+#include <linux/sched.h>
+#include <sys/time.h>	
+#include <sched.h>
+#include <sys/resource.h>
+#define STACK_SIZE 1000
+#define N 10000
 
-#define _BSD_SOURCE 
 int forking(int);
 int vforking(int);
-int cloning(void);
-int vcloning(void);
-void print_time_info(clock_t, struct tms *, struct tms *);
-
-int counter = 0;
-int main(int argc, char *argv[])
-{
-	/* Prepare to measure time */
-	struct tms start_tms, stop_tms;
-   	clock_t start_time, stop_time;
-   
-   /* Start measure time */ 
-   	if((start_time = times(&start_tms)) == -1)
-   		printf("Time error\n");
-
-   	/* Time started */
-   	
-	forking(10000);
+void cloning(int);
+void vcloning(int);
+void print_time_info(struct rusage parent, struct rusage child, struct timeval real_parent, struct timeval real_child);
+int process(); 
+int counter = 0; 
 
 
-	vforking(10000);
-   	/* Stop measure time */
-	if((stop_time = times(&stop_tms)) == -1)
-   		printf("Time error\n");
-   	
-   	/* Printing time info */
+int main(int argc, char *argv[]) { /* Prepare to measure time */
+	/* Prepare measure time! */
+	struct timeval tmp1, 
+		real_parent, 
+		real_child; 
+
+	gettimeofday(&real_parent, NULL);
+
+	/* Time started */
+ 		
+	 #ifdef FORK   	
+		 /* Forking tests */
+		forking(N);
+	#elif VFORK
+		/* Vforking tests */
+		vforking(N);
+   	#elif CLONE	
+		/* Cloning as forking test */
+		cloning(N);
+	#elif VCLONE
+		/* Cloning as vforking test */
+		vcloning(N);
+	#endif		
+  	/* Setting up finish time */
+  	gettimeofday(&tmp1,NULL);
+  	real_parent.tv_sec = tmp1.tv_sec - real_parent.tv_sec;
+  	real_parent.tv_usec = tmp1.tv_usec - real_parent.tv_usec;
+
+
+  	/* Collection information for parent process */ 
+  	struct rusage parent, child;
+  	getrusage(RUSAGE_SELF, &parent);
+  	getrusage(RUSAGE_CHILDREN, &child);
+
+
+  	/* Printing info */
+
    	printf("[Main] took: \n");
-   	print_time_info(stop_time - start_time, &start_tms, &stop_tms);
+  	print_time_info(parent, child, real_parent, real_child);
+
 
 }
 
 int forking(int n)
 {
-	/* Prepare to measure time */
-	struct tms start_tms, stop_tms;
-   	clock_t start_time, stop_time;
-   
-   /* Start measure time */ 
-   	if((start_time = times(&start_tms)) == -1)
-   		printf("Time error\n");
 
-   	/* Time started */
+	/* Prepare measure time! */
+	struct timeval tmp, tmp1, 
+		real_parent, 
+		real_child; 
+
+	gettimeofday(&real_parent, NULL);
+
+	/* Time started */
+
 
   	int status;	
-   	pid_t pid;
-   	for(int i = 0; i < n ; i++){
-   		if((pid = fork()) < 0)
-   			printf("Forking error!\n");
-   		else if(pid == 0){
-   			counter++;
-   			_exit(0);
-   		} else {
-   			waitpid(pid, &status, 0);
-   		}
+  	pid_t pid;
+  	for(int i = 0; i < n ; i++){
+			gettimeofday(&tmp, NULL);
+  		if((pid = fork()) < 0)
+  			printf("Forking error!\n");
+  		else if(pid == 0){
+  			counter++;
+  			_exit(0);
+  		} else {
+  			waitpid(pid, &status, 0);
+			gettimeofday(&tmp1,	NULL);
+  			real_child.tv_sec += tmp1.tv_sec - tmp.tv_sec;
+  			real_child.tv_usec += tmp1.tv_usec - tmp.tv_usec;
+  		}
+  	}
 
-   	}
-   	printf("[Forking fun] Counter = %d\n",counter);
+  	/* Setting up finish time */
+  	gettimeofday(&tmp1,NULL);
+  	real_parent.tv_sec = tmp1.tv_sec - real_parent.tv_sec;
+  	real_parent.tv_usec = tmp1.tv_usec - real_parent.tv_usec;
 
-   	/* Stop measure time */
-	if((stop_time = times(&stop_tms)) == -1)
-   		printf("Time error\n");
-   	
-   	/* Printing time info */
-   	print_time_info(stop_time - start_time, &start_tms, &stop_tms);
 
-   	return 0;
+  	/* Collection information for parent process */ 
+  	struct rusage parent, child;
+  	getrusage(RUSAGE_SELF, &parent);
+  	getrusage(RUSAGE_CHILDREN, &child);
+
+
+  	/* Printing info */
+
+  	printf("[Forking fun] Counter = %d\n", counter);
+  	print_time_info(parent, child, real_parent, real_child);
+
+
+  	return 0;
 }	
 int vforking(int n)
 {
-	/* Prepare to measure time */
-	struct tms start_tms, stop_tms;
-   	clock_t start_time, stop_time;
-   
-   /* Start measure time */ 
-   	if((start_time = times(&start_tms)) == -1)
-   		printf("Time error\n");
+	/* Prepare measure time! */
+	struct timeval tmp, tmp1, 
+		real_parent, 
+		real_child; 
 
-   	/* Time started */
+	gettimeofday(&real_parent, NULL);
+
+	/* Time started */
 
   	int status;	
    	pid_t pid;
    	for(int i = 0; i < n ; i++){
+
+   		gettimeofday(&tmp, NULL);
    		if((pid = vfork()) < 0)
    			printf("vForking error!\n");
    		else if(pid == 0){
@@ -99,28 +139,152 @@ int vforking(int n)
    			_exit(0);
    		} else {
    			waitpid(pid, &status, 0);
+   			gettimeofday(&tmp1,	NULL);
+  			real_child.tv_sec += tmp1.tv_sec - tmp.tv_sec;
+  			real_child.tv_usec += tmp1.tv_usec - tmp.tv_usec;
    		}
 
    	}
-   	printf("[Vforking fun] Counter = %d\n",counter);
 
-   	/* Stop measure time */
-	if((stop_time = times(&stop_tms)) == -1)
-   		printf("Time error\n");
-   	
-   	/* Printing time info */
-   	print_time_info(stop_time - start_time, &start_tms, &stop_tms);
+  	/* Printing info */
+  	gettimeofday(&tmp1,NULL);
+  	real_parent.tv_sec = tmp1.tv_sec - real_parent.tv_sec;
+  	real_parent.tv_usec = tmp1.tv_usec - real_parent.tv_usec;
+
+
+  	/* Collection information for parent process */ 
+  	struct rusage parent, child;
+  	getrusage(RUSAGE_SELF, &parent);
+  	getrusage(RUSAGE_CHILDREN, &child);
+
+
+  	/* Printing info */
+
+   	printf("[Vforking fun] Counter = %d\n",counter);
+  	print_time_info(parent, child, real_parent, real_child);
 
    	return 0;
 }	
 
-void print_time_info(clock_t clock, struct tms *start_time, struct tms *current_time){
-
-	long ticks = sysconf(_SC_CLK_TCK);
-
-	double user, sys;
-	printf("\t\tReal time is:  				\t%.2f\n", clock/ (double) ticks);
-	printf("\t\tUser time is: 				\t%.2f\n",(user = ( current_time->tms_utime - start_time->tms_utime )/(double) ticks) );
-	printf("\t\tSystem time is:				\t%.2f\n",(sys = ( current_time->tms_stime - start_time->tms_stime )/(double) ticks));
-	printf("\t\tUser + System time time is:	\t\t%.2f\n",user+sys );
+int process(){
+	counter++;
+	_exit(0);
 }
+
+void cloning(int n){
+	/* Prepare measure time! */
+	struct timeval tmp, tmp1, 
+		real_parent, 
+		real_child; 
+
+	gettimeofday(&real_parent, NULL);
+
+	/* Time started */
+
+
+
+
+
+
+   char *stack;
+   stack =(char*) malloc(STACK_SIZE);
+   stack += STACK_SIZE;
+   int status;
+   for(int i = 0; i < n ; i++){
+  	 	gettimeofday(&tmp, NULL);
+    	clone(&process,stack ,SIGCHLD, (char*) NULL );
+    	wait(&status);
+    	gettimeofday(&tmp1,	NULL);
+  		real_child.tv_sec += tmp1.tv_sec - tmp.tv_sec;
+  		real_child.tv_usec += tmp1.tv_usec - tmp.tv_usec;
+   }
+   
+  	/* Printing info */
+  	gettimeofday(&tmp1,NULL);
+  	real_parent.tv_sec = tmp1.tv_sec - real_parent.tv_sec;
+  	real_parent.tv_usec = tmp1.tv_usec - real_parent.tv_usec;
+
+
+  	/* Collection information for parent process */ 
+  	struct rusage parent, child;
+  	getrusage(RUSAGE_SELF, &parent);
+  	getrusage(RUSAGE_CHILDREN, &child);
+
+
+  	/* Printing info */
+
+   	printf("[Cloning fun] Counter = %d\n",counter);
+  	print_time_info(parent, child, real_parent, real_child);
+
+}
+
+void vcloning(int n){
+	/* Prepare measure time! */
+	struct timeval tmp, tmp1, 
+		real_parent, 
+		real_child; 
+
+	gettimeofday(&real_parent, NULL);
+
+	/* Time started */
+
+   int status;
+   char *stack;
+   stack =(char*) malloc(STACK_SIZE);
+   stack += STACK_SIZE;
+   for(int i = 0; i < n ; i++){
+   		gettimeofday(&tmp, NULL);
+	   	clone(&process,stack , SIGCHLD | CLONE_VM | CLONE_VFORK, (char*) NULL );
+   		wait(&status);
+   		gettimeofday(&tmp1,	NULL);
+  		real_child.tv_sec += tmp1.tv_sec - tmp.tv_sec;
+  		real_child.tv_usec += tmp1.tv_usec - tmp.tv_usec;
+   }
+   
+	  	/* Printing info */
+  	gettimeofday(&tmp1,NULL);
+  	real_parent.tv_sec = tmp1.tv_sec - real_parent.tv_sec;
+  	real_parent.tv_usec = tmp1.tv_usec - real_parent.tv_usec;
+
+
+  	/* Collection information for parent process */ 
+  	struct rusage parent, child;
+  	getrusage(RUSAGE_SELF, &parent);
+  	getrusage(RUSAGE_CHILDREN, &child);
+
+
+  	/* Printing info */
+
+   	printf("[VCloning fun] Counter = %d\n",counter);
+  	print_time_info(parent, child, real_parent, real_child);
+
+}
+
+
+
+void print_time_info(struct rusage parent, struct rusage child, struct timeval real_parent, struct timeval real_child){
+
+	struct timeval sys_parent, usr_parent, sys_child, usr_child;	
+	sys_parent.tv_sec = parent.ru_stime.tv_sec;
+  	sys_parent.tv_usec = parent.ru_stime.tv_usec;
+  	usr_parent.tv_sec = parent.ru_utime.tv_sec;
+  	usr_parent.tv_usec = parent.ru_utime.tv_usec;
+
+  	/* Collecting times from childs */
+
+  	sys_child.tv_sec = child.ru_stime.tv_sec;
+  	sys_child.tv_usec = child.ru_stime.tv_usec;
+  	usr_child.tv_sec = child.ru_utime.tv_sec;
+  	usr_child.tv_usec = child.ru_utime.tv_usec;
+
+
+  	printf("\t Parent sys time:     %f\n",(float)(sys_parent.tv_sec + sys_parent.tv_usec/1000000.0) );
+  	printf("\t Parent usr time:     %f\n",(float)(usr_parent.tv_sec + usr_parent.tv_usec/1000000.0) );
+  	printf("\t Parent real time:    %f\n",(float)(real_parent.tv_sec + real_parent.tv_usec/1000000.0) );
+
+  	printf("\t Childrens sys time:  %f\n",(float)(sys_child.tv_sec + sys_parent.tv_usec/1000000.0) );
+  	printf("\t Childrens usr time:  %f\n",(float)(usr_child.tv_sec + usr_parent.tv_usec/1000000.0) );
+  	printf("\t Childrens real time: %f\n",(float)(real_child.tv_sec + real_parent.tv_usec/1000000.0) );
+}
+
+
